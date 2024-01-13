a535657a6d40557503dad6ea7f548b0f3f407ce5
wwoi03 <tuandao8826848@gmail.com>
2024-01-13 14:37:06 +0700

    connect database 1

--- MTKPM-QuanLyKhachSan/Models/DatabaseContext.cs
+++ MTKPM-QuanLyKhachSan/Models/DatabaseContext.cs
@@ -1,6 +1,31 @@
-﻿namespace MTKPM_QuanLyKhachSan.Models
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace MTKPM_QuanLyKhachSan.Models
 {
-    public class DatabaseContext
+    public class DatabaseContext : DbContext
     {
+        public DatabaseContext(DbContextOptions options) : base(options)
+        {
+
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<EmployeeRole>()
+                .HasKey(o => new { o.RoleId, o.EmployeeId });
+            modelBuilder.Entity<Order>()
+                .HasKey(o => new { o.BookRoomId, o.ServiceId });
+        }
+
+        public DbSet<Employee> Employees { get; set; }
+        public DbSet<Customer> Customers { get; set; }
+        public DbSet<Room> Rooms { get; set; }
+        public DbSet<RoomType> RoomTypes { get; set; }
+        public DbSet<Bill> Bills { get; set; }
+        public DbSet<BookRoom> BookRooms { get; set; }
+        public DbSet<Order> Orders { get; set; }
+        public DbSet<Service> Services { get; set; }
+        public DbSet<Role> Roles { get; set; }
+        public DbSet<EmployeeRole> EmployeeRoles { get; set; }
     }
 }
--- MTKPM-QuanLyKhachSan/Program.cs
+++ MTKPM-QuanLyKhachSan/Program.cs
@@ -1,7 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MTKPM_QuanLyKhachSan.Models;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+
+// Kết nối đến database
+builder.Services.AddDbContext<DatabaseContext>(x => x.UseSqlServer(builder.Configuration.GetConnectionString("DbContext")));
+
+// Kích hoạt Session
+builder.Services.AddSession();
 
 var app = builder.Build();
 
@@ -12,6 +21,9 @@ if (!app.Environment.IsDevelopment())
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
+
+// sử dụng session
+app.UseSession();
 
 app.UseHttpsRedirection();
 app.UseStaticFiles();
