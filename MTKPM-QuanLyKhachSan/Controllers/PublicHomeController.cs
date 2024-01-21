f57ca1bd1b30caf8b31049d11f1418df310073d9
Bui Nguyen Nhat Phi <riotter2703@gmail.com>
2024-01-22 00:18:02 +0700

    Done Gửi thông tin KH điền form đến email của Phi

--- MTKPM-QuanLyKhachSan/Controllers/PublicHomeController.cs
+++ MTKPM-QuanLyKhachSan/Controllers/PublicHomeController.cs
@@ -2,6 +2,7 @@
 using Microsoft.Extensions.Configuration;
 using MimeKit;
 using MailKit.Net.Smtp;
+using MailKit.Security;
 
 namespace MTKPM_QuanLyKhachSan.Controllers
 {
@@ -26,32 +27,27 @@ namespace MTKPM_QuanLyKhachSan.Controllers
 		//Xử lý form thông tin
 		private void SendEmail(string name, string email, string subject, string message)
 		{
-			// Kiểm tra nếu name hoặc email là null
-			if (name == null || email == null)
-			{
-				// Xử lý trường hợp name hoặc email là null
-				// Ví dụ: Log lỗi hoặc thông báo người dùng
-				return;
-			}
-			
+			Console.Write(name);
 			var senderEmail = _configuration["thangdiensongovietnam@gmail.com"];
 			var senderPassword = _configuration["thangdien0123456789"];
 			var receiverEmail = _configuration["riotter2703@gmail.com"];
 
 			var emailMessage = new MimeMessage();
 			emailMessage.From.Add(new MailboxAddress(name, email));
-			emailMessage.To.Add(new MailboxAddress("Nhật Phi", receiverEmail));
+			emailMessage.To.Add(new MailboxAddress("Nhat Phi", "riotter2703@gmail.com"));
 			emailMessage.Subject = subject;
 
 			emailMessage.Body = new TextPart("plain")
 			{
-				Text = message
+				Text = $"From: {email} \n{message}"
 			};
 
 			using (var client = new SmtpClient())
 			{
-				client.Connect(_configuration["smtp.gmail.com"], int.Parse(_configuration["587"]), true);
-				client.Authenticate(senderEmail, senderPassword);
+				//client.Connect(_configuration["smtp.gmail.com"], int.Parse(_configuration["587"]), true);
+				client.ServerCertificateValidationCallback = (s, c, h, e) => true;
+				client.Connect("smtp.gmail.com", 587, SecureSocketOptions.StartTlsWhenAvailable);
+				client.Authenticate("thangdiensongovietnam@gmail.com", "ujyr rsoo ghrw qbkr");
 				client.Send(emailMessage);
 				client.Disconnect(true);
 			}
