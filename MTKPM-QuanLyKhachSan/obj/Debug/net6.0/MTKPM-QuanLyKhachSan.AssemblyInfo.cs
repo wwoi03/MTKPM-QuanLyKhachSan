73cc9115702eda193b3c7a0926ed37170ba01879
Bui Nguyen Nhat Phi <riotter2703@gmail.com>
2024-02-26 15:39:47 +0700

    done Xem Lịch sử đặt phòng

--- MTKPM-QuanLyKhachSan/obj/Debug/net6.0/MTKPM-QuanLyKhachSan.AssemblyInfo.cs
+++ MTKPM-QuanLyKhachSan/obj/Debug/net6.0/MTKPM-QuanLyKhachSan.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MTKPM-QuanLyKhachSan")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+eabca6d5f25b347a3685b6db8ff4f09b46611401")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fc462b70a1717e22140a36e26685b1ce33958948")]
 [assembly: System.Reflection.AssemblyProductAttribute("MTKPM-QuanLyKhachSan")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MTKPM-QuanLyKhachSan")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
