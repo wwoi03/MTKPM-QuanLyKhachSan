621af509db878bf3cf1e3a8533188d015fa030fb
Bui Nguyen Nhat Phi <riotter2703@gmail.com>
2024-03-04 15:26:48 +0700

    Liên hệ - gửi mail tới email khách sạn
    Done - Clean code

--- MTKPM-QuanLyKhachSan/Controllers/PublicHomeController.cs
+++ MTKPM-QuanLyKhachSan/Controllers/PublicHomeController.cs
@@ -12,6 +12,7 @@ namespace MTKPM_QuanLyKhachSan.Controllers
 
 		public PublicHomeController(IConfiguration configuration)
 		{
+			//Tạo dịch vụ configuration cho chức năng gửi email
 			_configuration = configuration;
 		}
 		//Hàm SendMessage để xử lý chức năng gửi tin nhắn
@@ -31,27 +32,37 @@ namespace MTKPM_QuanLyKhachSan.Controllers
 		private void SendEmail(string name, string email, string subject, string message)
 		{
 			Console.Write(name);
+			//Tạo tài khoản trung gian để nhận dữ liệu người dùng nhập và gửi mail tới Email của khách sạn
 			var senderEmail = _configuration["thangdiensongovietnam@gmail.com"];
 			var senderPassword = _configuration["thangdien0123456789"];
+			//Tạo tài khoản mặc định cho khách sạn sẽ nhận email từ người dùng
 			var receiverEmail = _configuration["riotter2703@gmail.com"];
-
+			//Sử dụng thư viện Mailkit và Mime để xây dựng chức năng Send Email
 			var emailMessage = new MimeMessage();
+			//Tên người gửi và địa chỉ email gửi thư
 			emailMessage.From.Add(new MailboxAddress(name, email));
+			//Tên người nhận và địa chỉ email nhận thư
 			emailMessage.To.Add(new MailboxAddress("Nhat Phi", "riotter2703@gmail.com"));
+			//Tên chủ đề của email
 			emailMessage.Subject = subject;
-
+			//Phần thân chứa nội dung của email
 			emailMessage.Body = new TextPart("plain")
 			{
 				Text = $"From: {email} \n{message}"
 			};
 
+			//Tạo tài khoản mặc định sẽ nhận email từ khách hàng
 			using (var client = new SmtpClient())
 			{
-				//client.Connect(_configuration["smtp.gmail.com"], int.Parse(_configuration["587"]), true);
+				//Xây dựng cấu trúc chuẩn của thư viện MailKit như sau:
+				//Kết nối Server để sử dụng dịch vụ của Gmail
 				client.ServerCertificateValidationCallback = (s, c, h, e) => true;
 				client.Connect("smtp.gmail.com", 587, SecureSocketOptions.StartTlsWhenAvailable);
+				//Xác thực và mặc định tài khoản sẽ là trung gian nhận thông tin từ form và gửi mail đến email của khách sạn
 				client.Authenticate("thangdiensongovietnam@gmail.com", "ujyr rsoo ghrw qbkr");
+				//Thực hiện chức năng gửi Email
 				client.Send(emailMessage);
+				//Kết thúc sau khi đã gửi Email thành công
 				client.Disconnect(true);
 			}
 		}
