755e22d80a3e0328da98df6f4828ac3d2d5b4dd6
Bui Nguyen Nhat Phi <riotter2703@gmail.com>
2024-03-04 18:49:05 +0700

    .

--- MTKPM-QuanLyKhachSan/obj/Debug/net6.0/MTKPM-QuanLyKhachSan.AssemblyInfo.cs
+++ MTKPM-QuanLyKhachSan/obj/Debug/net6.0/MTKPM-QuanLyKhachSan.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MTKPM-QuanLyKhachSan")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a06061db96239d096c343ceb8477970b91844988")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9fba8aaf3652712eba76acaaa0a24bef19ec24e3")]
 [assembly: System.Reflection.AssemblyProductAttribute("MTKPM-QuanLyKhachSan")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MTKPM-QuanLyKhachSan")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
