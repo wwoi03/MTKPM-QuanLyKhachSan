aa57ef5d53a96846244aff6fbe2fe0ab3d91f982
wwoi03 <tuandao8826848@gmail.com>
2024-03-25 02:08:08 +0700

    fix layout

---
+++ MTKPM-QuanLyKhachSan/Areas/Admin/DesignPattern/ProxyProtected/Controllers/AdminBookingProxyController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MTKPM_QuanLyKhachSan.Areas.Admin.DesignPattern.ProxyProtected.Controllers
+{
+    public class AdminBookingProxyController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
